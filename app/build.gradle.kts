c3d22ccb938bda0996b7b55f209f1cf12afabb8e
nadav2 <nadavkitron123@gmail.com>
2026-04-11 18:20:57 +0300

    v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.raccoonwallet.app"
         minSdk = 31
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.1.2"
+        versionCode = 13
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
